94a86ad4d5d37b4b5faa74a2eaa986f1d6b93a69
Gabor Buella <gbuella@gmail.com>
2015-06-06 17:25:34 +0200

    Add graceful failure in case of missing mpg123 on build host

--- ext/portaudio/extconf.rb
+++ ext/portaudio/extconf.rb
@@ -7,10 +7,24 @@ unless have_header('portaudio.h')
   exit
 end
 
+unless have_header('mpg123.h')
+  puts "mpg123.h not found"
+  puts "please brew install mpg123"
+  puts "or apt-get install libmpg123-dev"
+  exit
+end
+
 unless have_library('portaudio')
   puts "lib portaudio not found"
   puts "brew install portaudio"
   puts "or apt-get install portaudio19-dev"
+  exit
+end
+
+unless have_library('mpg123')
+  puts "lib mpg123 not found"
+  puts "please brew install mpg123"
+  puts "or apt-get install libmpg123-0"
   exit
 end
 
